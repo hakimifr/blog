c7cb338092c626b1ce0b03e52b50af136b689bfd
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2026-03-19 23:34:57 +0800

    initial commit with xfs blog

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://hakimifr.github.io',
+  base: '/blog',
+  integrations: [
+    starlight({
+      title: 'Hakimi\'s blog',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/hakimifr' }],
+      customCss: ['./src/styles/custom.css'],
+      sidebar: [
+        // {
+        //   label: 'Guides',
+        //   items: [
+        //     // Each item here is one entry in the navigation menu.
+        //     { label: 'Example Guide', slug: 'guides/example' },
+        //   ],
+        // },
+        {
+          label: 'Blog Content',
+          autogenerate: { directory: 'contents' },
+        },
+      ],
+    }),
+  ],
+});
